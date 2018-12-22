a746455ee195969036c1d6aa4ff7c4fc6c8640fe
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2018-12-22 12:57:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const antic = JSON.parse(fs.readFileSync('./antic.json', 'utf8'));
+const antic = JSON.parse)fs.readFileSync('./antic.json', 'utf8'));
 client.on("message", message =>{
 if(!antic[message.author.id]) {
 antic[message.author.id] = {
